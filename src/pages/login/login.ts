5c117f38d075010621acf1e4bf04f5b4f0f78bc5
Alfonso Hernández Montoya <ahernandezmo@calymayor.com.mx>
2017-11-06 09:47:23 -0600

    6 de noviembre

---
+++ src/pages/login/login.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Http, Headers } from '@angular/http'
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SignupPage } from '../signup/signup'
+import { HomePage } from '../home/home'
+import { TodosProvider } from '../../providers/todos/todos'
+
+
+@IonicPage()
+@Component({
+	selector: 'page-login',
+	templateUrl: 'login.html',
+})
+export class LoginPage {
+	username: string
+	password: string
+
+	constructor(public nav: NavController, public http: Http, public todoService: TodosProvider) {}
+
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad LoginPage');
+	}
+
+	login() {
+		let headers = new Headers()
+		headers.append('Content-Type', 'application/json')
+
+		let credentials = {
+			username: this.username,
+			password: this.password
+		}
+
+		this.http.post('http://localhost:3000/auth/login', JSON.stringify(credentials), { headers: headers })
+		.subscribe(res => {
+			this.todoService.init(res.json())
+			this.nav.setRoot(HomePage)
+		}, (err) => {
+			console.log(err)
+		})
+	}
+
+	launchSignup() {
+		this.nav.push(SignupPage)
+	}
+}
